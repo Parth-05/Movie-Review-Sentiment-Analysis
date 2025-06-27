7d0ae4070a9cf9d9eae27f9f50dd4ef45ea21dfa
Parth-05 <parthmarathe5@gmail.com>
2025-06-26 23:13:07 -0700

    Added changes for handling cross-origins

--- sentiment_api/app.py
+++ sentiment_api/app.py
@@ -1,12 +1,26 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from transformers import pipeline
 
-# Load your fine-tuned model from Hugging Face
+# Load model
 model_name = "Parth05/finetuning-sentiment-model-3000-samples"
 classifier = pipeline("text-classification", model=model_name)
 
 app = FastAPI()
+
+#  Get CORS origins from environment variable
+cors_origins = os.getenv("CORS_ORIGINS", "")
+origins = cors_origins.split(",") if cors_origins else []
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class TextInput(BaseModel):
     text: str
